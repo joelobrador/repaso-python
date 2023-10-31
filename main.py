7a64f5989cb79e1b82334966875dbdd225a983b0
Joel <al.joelobrador@iesramonllull.net>
2023-10-31 11:33:37 +0100

    mi primer commit

---
+++ main.py
@@ -0,0 +1,47 @@
+print("Hola mundo!! Soy Joel Obrador WEB :)") 
+print("Hola mundo 1!!")
+
+# Soy un comentario
+print("Hola mundo 2!!")
+
+"""
+Texto que no se va interpetrar
+Texto que no se va interpetrar
+Texto que no se va interpetrar
+"""
+# Varibles
+texto = "Repaso de Python con Victor"
+nombre = "Victor Robles"
+altura = "179"
+year = 2023
+
+print(f"{texto} -{nombre} - {altura} - {year} ")
+print(texto + " - " nombre + " - " + altura + " - " + str(year))
+
+# Entrada
+sitioweb = int(input("¿Cual es tu pagina web?: "))
+print("El sitio web del usuario es: " + sitioweb)
+#Condiciones
+altura = 179
+
+if altura >= 179:
+    print("Eres una persona alta!")
+else:
+    print("Eres BAJITO!!")
+
+#Funciones
+
+def mostrarAltura():
+    altura = int(input("¿Cual es tu altura?: "))
+    if altura >= 180
+        print("Eres una persona alta!!")
+    else:
+        print("Eres BAJITO")
+    return resultado
+
+print(mostrarAltura(var_altura))
+personas = "Victor", "Paco", "Pepe"
+print(personas[2])    
+
+for  personas in personas:
+    print("-" + persona)
